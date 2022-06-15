5cf58afbff26e0aef9b6a795ebbb063200da6225
Efren <jgar05202415@gmail.com>
2022-06-14 20:35:26 -0500

    eliminacion de nav

--- src/App.js
+++ src/App.js
@@ -21,12 +21,7 @@ function App() {
 
     {/*Barra de navegación*/}
     <nav className="nav">
-        <ul>
-            <li><a href="#">Inicio</a></li>
-            <li><a href="#">Peliculas</a></li>
-            <li><a href="#">Blog</a></li>
-            <li><a href="#">Contacto</a></li>
-        </ul>
+        
     </nav>
 
     {/*Contenido principal*/}
